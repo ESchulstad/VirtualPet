50ead91f83d2cbac7e17c3a7de0d2f1f13bf6a4f
ESchulstad <eschulstad@gmail.com>
2016-09-17 17:48:46 -0400

    Initial Commit

---
+++ VirtualPet/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualPet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome! Here is your virtual pet owl!");
+
+            VirtualPet owl = new VirtualPet(10,10,10);
+
+            
+
+        }
+    }
+}
---
+++ VirtualPet/VirtualPet.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualPet
+{
+    class VirtualPet
+    {
+
+        //fields
+
+        private string name;
+        private int hunger;
+        private int waste;
+        private int boredom;
+
+
+        //properties
+
+        public string Name
+        {
+            get { return this.name; }
+            set { this.name = value; }
+        }
+        
+        public int Hunger
+        {
+            get { return this.hunger; }
+            set { this.hunger = value; }
+        }
+
+        public int Waste
+        {
+            get { return this.waste; }
+            set { this.waste = value; }
+        }
+
+        public int Boredom
+        {
+            get { return this.boredom; }
+            set { this.boredom = value; }
+        }
+
+        //constructors
+
+        public VirtualPet(int hunger, int waste, int boredom)
+        {
+            this.hunger = hunger;
+            this.waste = waste;
+            this.boredom = boredom;
+        }
+
+        //methods
+
+        public void Feed()
+        {
+            hunger = hunger - 5;
+            waste = waste + 5;
+        }
+
+        public void Bathroom()
+        {
+            waste = waste - 10;
+            boredom = boredom + 5;
+        }
+
+        public void Play()
+        {
+            boredom = boredom - 7;
+            hunger = hunger + 10;
+        }
+
+        public void Tick()
+        {
+
+        }
+    }
+}
